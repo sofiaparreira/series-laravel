3a81394f8eefe6595c710b1a0f0cd53d0da13213
unknown <sofiapparreira@gmail.com>
2024-06-17 19:33:05 -0300

    firs commit

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SeriesController extends Controller
+{
+    public function listarSeries() {
+        $series = [
+            'Grays Anatomy',
+            'Pretty Little Liers',
+            'SWAT'
+        ];
+
+        $html = "<ul>";
+        foreach ($series as $serie) {
+            $html .= "<li>$serie</li>";
+        }
+        $html .= "</ul>";
+
+        echo $html;
+
+        
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\SeriesController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/series', [SeriesController::class, 'listarSeries']);
